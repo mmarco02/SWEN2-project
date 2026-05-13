4bc0861c10c61edf7c4e831c41db9ce334a1772c
mmarco <mmarco.molnar@gmail.com>
2026-05-13 12:22:16 +0200

    renamed Endpoint for tours

--- src/main/java/fh/swen/swen2tourplanner/controller/TourController.java
+++ src/main/java/fh/swen/swen2tourplanner/controller/TourController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/tour")
+@RequestMapping("/tours")
 @RequiredArgsConstructor
 class TourController {
 
